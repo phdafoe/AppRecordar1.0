e43aaa81843c4fd7816b299950256b8d668e1480
Andres <phdafoe@gmail.com>
2017-01-06 11:54:54 +0100

    Mejoras de Imagenes / Nuevas Clases de VC / de Cell y creación de singleton para salvado de Datos

---
+++ AppRecordar1.0/APIUTils.swift
@@ -0,0 +1,19 @@
+//
+//  APIUTils.swift
+//  AppRecordar1.0
+//
+//  Created by User on 6/1/17.
+//  Copyright © 2017 icologic. All rights reserved.
+//
+
+import Foundation
+
+let CONSTANTES = Constantes()
+
+
+struct Constantes {
+    
+    let KEY_TAREAS = "tasks"
+    let USER_DEFAULT = UserDefaults.standard
+    
+}
